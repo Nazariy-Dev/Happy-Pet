5c8b39653f871bc83042f3699ecb0eb1c57d3af5
Nazariy-Dev <139453365+Nazariy-Dev@users.noreply.github.com>
2024-05-01 22:02:52 +0300

    Autoprefixer fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import autoprefixer from 'autoprefixer'
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [
-        autoprefixer({}) // add options if needed
-      ],
-    }
-  }
+      plugins: [autoprefixer],
+    },
+  },
 })
